af2578a9133aa657bf73d6f3fd17c3c8935fc11a
jdiminic <jdiminic@gmail.com>
2025-03-23 14:07:08 +0100

    Save all

--- web3/block-game/test/testGameContracts.js
+++ web3/block-game/test/testGameContracts.js
@@ -6,22 +6,27 @@ describe("\\Game Contracts\\", function () {
 
   beforeEach(async function () {
     [owner, addr1] = await ethers.getSigners();
+    console.log("owner:", owner.address);
 
     GameToken = await ethers.getContractFactory("GameToken");
-    gameToken = await GameToken.deploy(ethers.utils.parseEther("1000"));
-    await gameToken.deployed();
+
+    gameToken = await GameToken.deploy(ethers.parseEther("1000"), {gasLimit: 3000000});
+    await gameToken.waitForDeployment();
+    if (!gameToken.address) throw new Error("GameToken deployment failed");
+
 
     GameMap = await ethers.getContractFactory("GameMap");
     gameMap = await GameMap.deploy(gameToken.address, owner.address);
-    await gameMap.deployed();
+    await gameMap.waitForDeployment();
+    if (!gameMap.address) throw new Error("GameMap deployment failed");
   });
 
   it("should allow buying a plot with valid payment", async function () {
     // Transfer tokens to addr1
-    await gameToken.transfer(addr1.address, ethers.utils.parseEther("200"));
+    await gameToken.transfer(addr1.address, ethers.parseEther("200"));
 
     // Approve and buy a plot
-    await gameToken.connect(addr1).approve(gameMap.address, ethers.utils.parseEther("200"));
+    await gameToken.connect(addr1).approve(gameMap.address, ethers.parseEther("200"));
     await gameMap.connect(addr1).buyPlot(0);
 
     // Check ownership
